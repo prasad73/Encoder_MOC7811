8ff3c31021f6dc7acc8992d94cd79219ce704f39
prasad73 <prasad94@gmail.com>
2017-09-13 16:33:16 +0530

    Add files via upload

---
+++ encoder0.ino
@@ -0,0 +1,32 @@
+const int pinA=2;//ref 
+unsigned int lastCount=0;
+volatile int c=0;
+void isr1()
+{      
+ static unsigned long lastInterruptTime=0;
+ unsigned long interruptTime=millis();
+ if(interruptTime-lastInterruptTime>5)
+ {
+  c++;
+ }
+ lastInterruptTime=interruptTime;
+}
+void setup()
+{
+  pinMode(pinA,INPUT);
+  attachInterrupt(digitalPinToInterrupt(pinA),isr1,RISING); 
+  Serial.begin(9600);
+  Serial.println("start");
+ } 
+ 
+ 
+ void loop()
+ {
+  if(c!=lastCount)
+  {
+    Serial.println(c);
+    lastCount=c;
+  }
+  if(c>360)
+  c=0;
+ }
